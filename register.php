10c31b3bb1457fa077e3a71e32422f98452ef131
aldder <aldo.deramo@gmail.com>
2013-12-16 11:52:40 +0100

    0.1
    motore di template

---
+++ register.php
@@ -0,0 +1,19 @@
+<?php
+
+require "include/template2.inc.php";
+require "include/dbms.inc.php";
+
+
+$header = new Template("skins/progettotdw/dtml/header.html");
+$index = new Template("skins/progettotdw/dtml/register.html");
+$footer = new Template("skins/progettotdw/dtml/footer.html");
+
+$main = new Template("skins/progettotdw/dtml/blank_page.html");
+$main -> setContent("header", $header->get());
+$main -> setContent("index", $index->get());
+$main -> setContent("footer", $footer->get());
+
+$main->close();
+
+
+?>
