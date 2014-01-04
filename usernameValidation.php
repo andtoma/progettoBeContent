27742bec0fb547b910ea4e1566acfc72f8b11bf4
loradd <addazi.lorenzo@gmail.com>
2014-01-04 04:11:41 +0100

    0.11
    Validazione Form di Registrazione tramite ajax ed espressioni regolari.

---
+++ usernameValidation.php
@@ -0,0 +1,8 @@
+<?
+require "include/dbms.inc.php";
+
+$result = mysql_query("select * from users where username='{$_REQUEST['username']}'");
+$num_rows = mysql_num_rows($result);
+// Should show you an integer result.
+echo $num_rows;
+?>
